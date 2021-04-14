b452eb654045e7cb2faf6c169efd95aa941bd961
Nagashreevsm <68920753+Nagashreevsm@users.noreply.github.com>
2021-04-14 18:31:39 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="Styles/1.css" type="text/css" >
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <title>Basic Banking System</title>
+
+</head>
+
+<body>
+ 
+  <div class="bgimage">
+     <div class="menu">
+        <div class="leftmenu">
+           <h4>TSF BANK</h4>
+        </div>
+        <div class="rightmenu">
+         <ul>
+         <li><a href="index.php">HOME</a></li>
+         <li><a href="createuser.php">Create User</a></li>
+         <li><a href="transfermoney.php">Transfer Money</a></li>
+         <li><a href="transactionhistory.php">Transaction History</a></li>
+         </ul>  
+        </div>
+      </div><br><br><br>
+    <div class="text">
+       <p>Banks are generally considered by most people to be utilities that allow the<br>transmission of value on a
+daily basis in modern society, but they also seem to<br> create devastating events like credit crises by the
+manufacture of credit.</p><br>
+<p> The evolution of the bank, of institutions for organizing the savings of groups of humans has had
+a number of separate points of origin in history in various societies in antiquity and most recently during
+the Middle Ages in Europe.</p>
+    </div>
+  </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
